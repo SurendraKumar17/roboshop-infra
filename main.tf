f7b943f26e4e9d114435d6bb96644577b132ec58
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-09-09 13:16:42 +0530

    .....

--- main.tf
+++ main.tf
@@ -1,44 +1,110 @@
 module "vpc" {
-  source         = "github.com/SurendraKumar17/tf-module-vpc"
+  source         = "github.com/raghudevopsb70/tf-module-vpc"
   env            = var.env
+  default_vpc_id = var.default_vpc_id
+
   for_each          = var.vpc
   cidr_block        = each.value.cidr_block
-  default_vpc_id    = var.default_vpc_id
   public_subnets    = each.value.public_subnets
   private_subnets   = each.value.private_subnets
   availability_zone = each.value.availability_zone
 }
 
 
+module "docdb" {
+  source = "github.com/raghudevopsb70/tf-module-docdb"
+  env    = var.env
 
-## This is for servers. For Mutable & Immutable
-# module "apps" {
-#   source = "github.com/SurendraKumar17/tf-module-app"
+  for_each            = var.docdb
+  subnet_ids          = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
+  vpc_id              = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  allow_cidr          = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
+  engine_version      = each.value.engine_version
+  number_of_instances = each.value.number_of_instances
+  instance_class      = each.value.instance_class
+}
+
+# module "rds" {
+#   source = "github.com/raghudevopsb70/tf-module-rds"
 #   env    = var.env
 #
-#   depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
-#
-#   for_each          = var.apps
-#   subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
-#   vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-#   allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
-#   alb               = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
-#   listener          = lookup(lookup(module.alb, each.value.alb, null), "listener", null)
-#   alb_arn           = lookup(lookup(module.alb, each.value.alb, null), "alb_arn", null)
-#   component         = each.value.component
-#   app_port          = each.value.app_port
-#   max_size          = each.value.max_size
-#   min_size          = each.value.min_size
-#   desired_capacity  = each.value.desired_capacity
-#   instance_type     = each.value.instance_type
-#   listener_priority = each.value.listener_priority
-#
-#   bastion_cidr = var.bastion_cidr
-# #   monitor_cidr = var.monitor_cidr
-#
+#   for_each            = var.rds
+#   subnet_ids          = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
+#   vpc_id              = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+#   allow_cidr          = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
+#   engine              = each.value.engine
+#   engine_version      = each.value.engine_version
+#   number_of_instances = each.value.number_of_instances
+#   instance_class      = each.value.instance_class
 # }
 #
-# // Load Test Machine
+#
+# module "elasticache" {
+#   source = "github.com/raghudevopsb70/tf-module-elasticache"
+#   env    = var.env
+#
+#   for_each        = var.elasticache
+#   subnet_ids      = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
+#   vpc_id          = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+#   allow_cidr      = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
+#   num_cache_nodes = each.value.num_cache_nodes
+#   node_type       = each.value.node_type
+#   engine_version  = each.value.engine_version
+# }
+#
+# module "rabbitmq" {
+#   source       = "github.com/raghudevopsb70/tf-module-rabbitmq"
+#   env          = var.env
+#   bastion_cidr = var.bastion_cidr
+#
+#   for_each   = var.rabbitmq
+#   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
+#   vpc_id     = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+#   allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
+# }
+
+//module "alb" {
+//  source = "github.com/raghudevopsb70/tf-module-alb"
+//  env    = var.env
+//
+//  for_each     = var.alb
+//  subnet_ids   = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
+//  vpc_id       = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+//  allow_cidr   = each.value.internal ? concat(lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "web", null), "cidr_block", null), lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)) : ["0.0.0.0/0"]
+//  subnets_name = each.value.subnets_name
+//  internal     = each.value.internal
+//  dns_domain   = each.value.dns_domain
+//}
+//
+
+## This is for servers. For Mutable & Immutable
+//module "apps" {
+//  source = "github.com/raghudevopsb70/tf-module-app"
+//  env    = var.env
+//
+//  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
+//
+//  for_each          = var.apps
+//  subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
+//  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+//  allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
+//  alb               = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
+//  listener          = lookup(lookup(module.alb, each.value.alb, null), "listener", null)
+//  alb_arn           = lookup(lookup(module.alb, each.value.alb, null), "alb_arn", null)
+//  component         = each.value.component
+//  app_port          = each.value.app_port
+//  max_size          = each.value.max_size
+//  min_size          = each.value.min_size
+//  desired_capacity  = each.value.desired_capacity
+//  instance_type     = each.value.instance_type
+//  listener_priority = each.value.listener_priority
+//
+//  bastion_cidr = var.bastion_cidr
+//  monitor_cidr = var.monitor_cidr
+//
+//}
+
+// Load Test Machine
 # resource "aws_spot_instance_request" "load" {
 #   instance_type          = "t3.medium"
 #   ami                    = "ami-068510c19de1f805d"
@@ -52,7 +118,7 @@ module "vpc" {
 #   key         = "Name"
 #   value       = "load-runner"
 # }
-
+#
 # resource "null_resource" "apply" {
 #   provisioner "remote-exec" {
 #     connection {
@@ -68,7 +134,7 @@ module "vpc" {
 #     ]
 #   }
 # }
-
+#
 
 //module "minikube" {
 //  source = "github.com/scholzj/terraform-aws-minikube"
--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "env" {}
 variable "vpc" {}
 variable "default_vpc_id" {}
-# variable "docdb" {}
+variable "docdb" {}
 # variable "rds" {}
 # variable "elasticache" {}
 # variable "rabbitmq" {}
