bac3e79925f9a77c875788142f48b5d9f3a454d5
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-09-14 22:48:33 +0530

    .....

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "env" {}
 variable "vpc" {}
 variable "default_vpc_id" {}
 variable "docdb" {}
-# variable "rds" {}
+variable "rds" {}
 # variable "elasticache" {}
 # variable "rabbitmq" {}
 # variable "alb" {}
