f550d2d6a7417535e48f84d1fb89fa34b34a64ee
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-09-16 15:22:16 +0530

    .....

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ module "apps" {
     subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
   vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
   allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
-  alb               = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
+#   alb               = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
 #   listener          = lookup(lookup(module.alb, each.value.alb, null), "listener", null)
 #   alb_arn           = lookup(lookup(module.alb, each.value.alb, null), "alb_arn", null)
   component         = each.value.component
